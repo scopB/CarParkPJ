5732d9b836f0f34a6e7a5398a826bd9eddee45e3
Pawat.thr <78230745+scopB@users.noreply.github.com>
2021-04-14 02:01:44 +0700

    update2

--- Back.py
+++ Back.py
@@ -9,11 +9,36 @@ mongo = PyMongo(app)
 myCollection1 = mongo.db.park
 myCollection2 = mongo.db.user
 
-@app.route('/create', methods=['POST'])
+class user_c:
+    def __init__(self,username,passwd):
+        self.username = username
+        self.passwd = passwd
+    def get_json(self):
+        return {'username':self.username,'passwd':self.passwd}
+
+user = {}
+
+def init():
+    query = myCollection2.find()
+    for i in query:
+        if i['username'] not in user:
+            user[i['username']]=user_c(i['username'],i['passwd'])
+
+init()
+
+# @app.route('/create', methods=['POST'])
+# def insert_one():
+#     data = request.json
+
+#     myCollection1.insert_one(data)
+#     return {'result': 'Created successfully'}
+
+@app.route('/register', methods=['POST'])
 def insert_one():
     data = request.json
-
-    myCollection1.insert_one(data)
+    passs = data['passwd'].encode()
+    hashed = bcrypt.hashpw(passs, bcrypt.gensalt())
+    myCollection2.insert_one({'username':data['username'],'passwd': hashed })
     return {'result': 'Created successfully'}
 
 
@@ -29,7 +54,25 @@ def find():
             "car": ele["car"],
             "idName": ele["idName"]
         })
+
     return { "result" : output }
+
+@app.route('/find_user',methods=['POST' , 'GET'])
+def fiuser():
+    data = request.json
+    if data['username'] in user:
+        name = data['username']
+        password = data['passwd'].encode()
+        data2 = myCollection2.find({'username': name})
+        he = data2['passwd'].encode()
+        # error
+        if bcrypt.checkpw(password, he ):
+            return {'result':1}
+        else:
+            return {'result':2}
+    else:
+        return {'result':0}
+
 
 @app.route('/reset', methods=['POST'])
 def reset():
